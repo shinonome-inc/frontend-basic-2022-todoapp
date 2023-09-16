9271b08b4ceca6138eeb227ca0026c10baea662a
higashiji <higashiji.dev@gmail.com>
2023-09-16 22:00:32 +0900

    storybookのバージョンアップ
    公式が提供している npx storybook@latest upgrade を利用
    https://storybook.js.org/docs/react/migration-guide

--- app/.storybook/main.js
+++ app/.storybook/main.js
@@ -1,4 +1,13 @@
 module.exports = {
   stories: ["../src/**/story.jsx"],
   addons: ["@storybook/addon-essentials"],
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
+  },
+
+  docs: {
+    autodocs: false,
+  },
 };
